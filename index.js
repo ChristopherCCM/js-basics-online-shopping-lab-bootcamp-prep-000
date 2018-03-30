a7f59d4833f358735f7d8491132e27a0bdf7447f
Christopher <christopherccm@msn.com>
2018-03-30 18:24:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,18 +71,13 @@ function removeFromCart(item) {
     }
 }
 
-//var total = total();
-
 function placeOrder(cardNumber) {
-  var totalStuff = total();
+  var finalTotal = total();
   console.log(cardNumber);
   if (cardNumber == null) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    //var total = total();
     cart = [];
-    return `Your total cost is $${totalStuff}, which will be charged to the card ${cardNumber}.`;
-    //return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    //cart.length = 0;
+    return `Your total cost is $${finalTotal}, which will be charged to the card ${cardNumber}.`;
   }
 }
