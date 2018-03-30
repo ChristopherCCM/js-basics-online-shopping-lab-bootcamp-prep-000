0130ab5716dd8822f694dc52f96fc0dddb9c5051
Christopher <christopherccm@msn.com>
2018-03-30 18:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     //var total = total();
+    cart = [];
     return `Your total cost is $${totalStuff}, which will be charged to the card ${cardNumber}.`;
     //return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     //cart.length = 0;
-    cart = [];
   }
 }
