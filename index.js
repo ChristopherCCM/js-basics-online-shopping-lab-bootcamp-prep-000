db1ec08b5651ff65dfd7ca538cca2cd7c64d4dc8
Christopher <christopherccm@msn.com>
2018-03-30 18:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,11 +72,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var finalTotal = total();
   console.log(cardNumber);
   if (cardNumber == null) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
+    var finalTotal = total();
     cart = [];
     return `Your total cost is $${finalTotal}, which will be charged to the card ${cardNumber}.`;
   }
